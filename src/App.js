74d40a0c8e235cfef43614fbb385e68cf4e74c87
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-04-13 13:34:15 +0200

    added icon svgs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
